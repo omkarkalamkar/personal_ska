cb196cbac09fdb8b83163bea3811ceea05cf9bca
muskansharma <muskans.ska@gmail.com>
2022-07-16 14:56:04 +0530

    HM-18:Modified Component Manager and Added Test Cases

--- tests/unit/test_setstandbylpmode_command.py
+++ tests/unit/test_setstandbylpmode_command.py
@@ -1,7 +1,7 @@
 import time
 
 import pytest
-from ska_tango_base.commands import ResultCode, TaskStatus
+from ska_tango_base.commands import TaskStatus
 from ska_tmc_common.test_helpers.helper_adapter_factory import (
     HelperAdapterFactory,
 )
@@ -10,40 +10,56 @@ from ska_tmc_dishleafnode.commands.setstandbylpmode_command import (
     SetStandbyLPMode,
 )
 from tests.mock_callable import MockCallable
-from tests.settings import create_cm, get_dishln_command_obj, logger
+from tests.settings import create_cm, logger
 
 
+@pytest.mark.long_running
 def test_setstandbylpmode_command(tango_context, dish_master_device):
-    _, set_standby_lp_mode_command, adapter_factory = get_dishln_command_obj(
-        SetStandbyLPMode
+    cm = create_cm(dish_master_device)
+    adapter_factory = HelperAdapterFactory()
+
+    setstandbylpmode_command = SetStandbyLPMode(
+        cm, cm.op_state_model, adapter_factory, logger
     )
-    assert set_standby_lp_mode_command.check_allowed()
-    (result_code, _) = set_standby_lp_mode_command.do()
-    assert result_code == ResultCode.OK
+    assert setstandbylpmode_command.check_allowed()
+    unique_id = f"{time.time()}_SetStandbyLPMode"
+    task_callback = MockCallable(unique_id)
+
+    task_status, _ = cm.setstandbylpmode(
+        setstandbylpmode_command, task_callback=task_callback
+    )
+    assert task_status == TaskStatus.QUEUED
+    assert task_callback.status == TaskStatus.QUEUED
+    time.sleep(0.1)
+    assert task_callback.status == TaskStatus.COMPLETED
     adapter = adapter_factory.get_or_create_adapter(dish_master_device)
     adapter.proxy.SetStandbyLPMode.assert_called_once_with()
 
 
-def test_setstandbylpmode_command_with_exception(
-    tango_context, dish_master_device
-):
+@pytest.mark.long_running
+def test_setstandbylpmode_command_exception(tango_context, dish_master_device):
     cm = create_cm(dish_master_device)
     adapter_factory = HelperAdapterFactory()
 
-    # include exception in SetStandbyLPMode command
     adapter_factory.get_or_create_adapter(
         dish_master_device, attrs={"SetStandbyLPMode.side_effect": Exception}
     )
-    set_standby_lp_mode_command = SetStandbyLPMode(
-        cm, cm.op_state_model, adapter_factory
+    setstandbylpmode_command = SetStandbyLPMode(
+        cm, cm.op_state_model, adapter_factory, logger
     )
-    assert set_standby_lp_mode_command.check_allowed()
-    (result_code, message) = set_standby_lp_mode_command.do()
-    assert result_code == ResultCode.FAILED
-    assert dish_master_device in message
+    assert setstandbylpmode_command.check_allowed()
+    unique_id = f"{time.time()}_SetStandbyLPMode"
+    task_callback = MockCallable(unique_id)
+
+    cm.setstandbylpmode(setstandbylpmode_command, task_callback=task_callback)
+    assert task_callback.status == TaskStatus.QUEUED
+    time.sleep(0.1)
+    assert task_callback.status == TaskStatus.FAILED
+    failed_message = "The invocation of the SetStandbyLPMode command is failed"
+    assert failed_message in task_callback.exception
 
 
-@pytest.mark.long_running2
+@pytest.mark.long_running
 def test_setstandbylpmode_command_adapter_none(
     tango_context, dish_master_device
 ):
