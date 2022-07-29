71c533be0b42b965c71f53b77fd6953e73ae7a7a
akshaysonawane <akshayss.ska@gmail.com>
2022-07-29 10:57:29 +0530

    HM-26 : Added test cases and resolve linting erros

--- tests/unit/test_commands.py
+++ tests/unit/test_commands.py
@@ -9,3 +9,8 @@ def test_setstandbyfpmode_command(dishln_device):
 def test_setstandbylpmode_command(dishln_device):
     result_code, _ = dishln_device.SetStandbyLPMode()
     assert result_code[0] == ResultCode.QUEUED
+
+
+def test_setoperatemode_command(dishln_device):
+    result_code, _ = dishln_device.SetOperateMode()
+    assert result_code[0] == ResultCode.QUEUED
