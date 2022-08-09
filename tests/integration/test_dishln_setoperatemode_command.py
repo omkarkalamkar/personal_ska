716d5c8d9b7f4805cb2b3548e41ed2a209d5cdc8
Bhushan Parakh <bhushan.ska@gmail.com>
2022-08-09 11:00:53 +0530

    HM-27: Fixed test issue

--- tests/integration/test_dishln_setoperatemode_command.py
+++ tests/integration/test_dishln_setoperatemode_command.py
@@ -36,6 +36,7 @@ def setoperatemode_command(tango_context, dishln_name, group_callback):
     )
     group_callback["longRunningCommandResult"].assert_change_event(
         (unique_id_fp[0], str(int(ResultCode.OK))),
+        lookahead=2,
     )
 
     result_op, unique_id_op = dish_leaf_node.SetOperateMode()
