15c0e9597fec7b05fa9ed0a10844b76e7b329a5d
Roshan Tayde <roshant.ska@gmail.com>
2023-03-31 17:21:37 +0530

    HM-185: Resolve poetry.lock issue on pipeline

--- temp_src/ska_tmc_dishleafnode/commands/utils.py
+++ temp_src/ska_tmc_dishleafnode/commands/utils.py
@@ -110,6 +110,34 @@ class UnitConverter:
         except SyntaxError as error:
             log_msg = f"Error while converting Deg:Min:Sec to decimal degrees.{error}"
             self.logger.error(log_msg)
+    
+    def dd_to_dms(argin):  # pylint: disable=inconsistent-return-statements
+        """
+        Converts a number in degree decimal to Deg:Min:Sec.
+
+        :param argin: A number in decimal degrees.
+            Example: 30.7129252
+        :return: Number in deg:min:sec format.
+            Example: 30:42:46.5307 is returned value for input 30.7129252.
+        """
+        try:
+            sign = 1
+            if argin < 0:
+                sign = -1
+            frac_min, degrees = math.modf(abs(argin))
+            frac_sec, minutes = math.modf(frac_min * 60)
+            seconds = frac_sec * 60
+            return f"{int(degrees * sign)}:{int(minutes)}:{round(seconds, 4)}"
+        
+    
+        except SyntaxError as error:
+            log_msg = (
+                f"Error while converting decimal degree to dig:min:sec.{error}"
+            )
+            logger.error(log_msg)
+    
+    
+
 
 
 class DishMode(enum.IntEnum):
