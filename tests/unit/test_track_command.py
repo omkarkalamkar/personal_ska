ce6ce670068046045a75d662ee4e29d6655dbfb3
yogeshk.ska <yogeshk.ska@gmail.com>
2023-04-06 13:33:34 +0530

    HM-186: unit tests added

--- tests/unit/test_track_command.py
+++ tests/unit/test_track_command.py
@@ -1,7 +1,9 @@
 from os.path import dirname, join
 
+import pytest
 from ska_tango_base.commands import ResultCode, TaskStatus
 from ska_tmc_common.enum import DishMode, PointingState
+from ska_tmc_common.exceptions import CommandNotAllowed
 
 from tests.settings import create_cm
 
@@ -33,3 +35,38 @@ def test_track_command_completed(
     task_callback.assert_against_call(
         call_kwargs={"status": TaskStatus.COMPLETED, "result": ResultCode.OK}
     )
+
+
+def test_track_command_adapter_none(task_callback, dish_master_device):
+    cm = create_cm(dish_master_device)
+    cm.update_device_dish_mode(DishMode.OPERATE)
+    assert cm.is_track_allowed()
+    track_input_str = get_track_input_str()
+    cm.track(track_input_str, task_callback=task_callback)
+
+    task_callback.assert_against_call(
+        call_kwargs={"status": TaskStatus.QUEUED}
+    )
+    task_callback.assert_against_call(
+        call_kwargs={"status": TaskStatus.IN_PROGRESS}
+    )
+    task_callback.assert_against_call(
+        status=TaskStatus.COMPLETED, result=ResultCode.FAILED
+    )
+
+
+def test_json_validation(tango_context, task_callback, dish_master_device):
+    cm = create_cm(dish_master_device)
+    cm.update_device_dish_mode(DishMode.OPERATE)
+    assert cm.is_track_allowed()
+    track_input_str = get_track_input_str("invalid_key_track.json")
+    result, message = cm.track(track_input_str, task_callback=task_callback)
+    assert result == ResultCode.FAILED
+    assert "key is not present" in message
+
+
+def test_track_command_not_allowed(tango_context, dish_master_device):
+    cm = create_cm(dish_master_device)
+    cm.update_device_dish_mode(DishMode.UNKNOWN)
+    with pytest.raises(CommandNotAllowed):
+        cm.is_track_allowed()
