9f5cf10b923f954d6961cce84f9d16e073cf6df3
Roshan Tayde <roshant.ska@gmail.com>
2023-04-07 15:00:31 +0530

    HM-188: Validate abort command implementation.

--- src/ska_tmc_dishleafnode/commands/abort_command.py
+++ src/ska_tmc_dishleafnode/commands/abort_command.py
@@ -4,10 +4,7 @@ Abort command class for DishLeafNode.
 from logging import Logger
 
 from ska_tango_base.commands import ResultCode
-from ska_tango_base.control_model import ObsState
 from ska_tmc_common.adapters import AdapterFactory
-from ska_tmc_common.enum import PointingState
-from ska_tmc_common.exceptions import InvalidObsStateError
 
 from ska_tmc_dishleafnode.commands.abstract_command import DishLNCommand
 from ska_tmc_dishleafnode.manager.component_manager import (
@@ -18,8 +15,7 @@ from ska_tmc_dishleafnode.manager.component_manager import (
 class Abort(DishLNCommand):
     """
     A class for DishLeafNode's Abort() command.
-
-    Command to abort the Dish Subarray and bring it to its ABORTED state.
+    Command to abort the Dish Master and bring it to its ABORTED state.
     """
 
     def __init__(
@@ -29,10 +25,12 @@ class Abort(DishLNCommand):
         adapter_factory: AdapterFactory,
         logger: Logger,
     ):
-        super().__init__(component_manager=component_manager, logger=logger)
-        self.op_state_model = op_state_model
-        self._adapter_factory = adapter_factory or AdapterFactory()
-        self.component_manager = component_manager
+        super().__init__(
+            component_manager,
+            op_state_model,
+            adapter_factory,
+            logger=logger,
+        )
 
     def check_allowed(self):
         """
@@ -44,40 +42,21 @@ class Abort(DishLNCommand):
         :return: True if this command is allowed
 
         :rtype: boolean
-
         """
 
-        """
-        Currently Abort command is allowed for all the dish modes.
-        To be decided .....
-        """
+        # dish manager allows abort in all the dish modes
+        # and pointing states
+        # DishMode/s & pointing state/s To be decided ....
+
+        self.check_op_state("Abort")
         self.component_manager.check_device_responsive()
-        self.component_manager.check_op_state("Abort")
-
-        current_obs_state = self.component_manager.get_device().obs_state
-
-        if current_obs_state not in (
-            ObsState.RESOURCING,
-            ObsState.CONFIGURING,
-            ObsState.SCANNING,
-            ObsState.IDLE,
-            ObsState.READY,
-        ):
-            message = (
-                "Abort command is not allowed in current observation"
-                + "on device {}".format(self.component_manager.dish_dev_name)
-                + "Reason: The current observation state for observation is"
-                + "{}".format(current_obs_state)
-                + 'The "Abort" command has NOT been executed. This device will'
-                + "continue with normal operation."
-            )
-            raise InvalidObsStateError(message)
 
         return True
 
+    # pylint: disable=arguments-differ
     def do(self):
         """
-        Invokes TrackStop command on the DishMaster.
+        Invokes AbortCommands command on the DishMaster.
 
         param argin:
             None
@@ -92,16 +71,15 @@ class Abort(DishLNCommand):
         """
         try:
             ret_code, message = self.init_adapter()
+
             if ret_code == ResultCode.FAILED:
                 return ret_code, message
-            if (
-                self.component_manager.get_device().pointing_state
-                is not PointingState.READY
-            ):
-                ret_code, message = self.call_adapter_method(
-                    "Dish Master", self.dish_master_adapter, "AbortCommands"
-                )
+
+            result_code = self.call_adapter_method(
+                "Dish Master", self.dish_master_adapter, "Abort"
+            )
             self.logger.info("Abort command executed successfully.")
+
         except Exception as e:
             self.logger.exception(f"Command invocation failed: {e}")
             return self.generate_command_result(
@@ -113,4 +91,4 @@ class Abort(DishLNCommand):
                 The command has NOT been executed.
                 This device will continue with its current operation.""",
             )
-        return ret_code, message
+        return result_code
