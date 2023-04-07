c9dce59d96b362902918230b465fb3a877749113
Roshan Tayde <roshant.ska@gmail.com>
2023-04-07 15:38:46 +0530

    HM-188: Validate abort command implementation.

---
+++ tests/integration/test_dishln_abort_command.py
@@ -0,0 +1,30 @@
+import pytest
+from ska_tango_base.commands import ResultCode
+from ska_tmc_common.dev_factory import DevFactory
+
+from tests.settings import DISH_MASTER_DEVICE, event_remover, logger
+
+
+def abort_dish_leaf_node(
+    tango_context,
+    group_callback,
+):
+
+    logger.info(f"{tango_context}")
+    dev_factory = DevFactory()
+    event_remover(
+        group_callback,
+        ["longRunningCommandsInQueue", "longRunningCommandResult"],
+    )
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    (result, _) = dish_master.AbortCommands()
+    assert result[0] == ResultCode.OK
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_abort_command(tango_context, group_callback):
+    abort_dish_leaf_node(
+        tango_context,
+        group_callback,
+    )
---
+++ tests/unit/test_abort_command.py
@@ -0,0 +1,31 @@
+import pytest
+from ska_tango_base.commands import ResultCode
+from ska_tmc_common.exceptions import DeviceUnresponsive
+
+from ska_tmc_dishleafnode.commands.abort_command import Abort
+from tests.settings import DISH_MASTER_DEVICE, get_dishln_command_obj, logger
+
+
+@pytest.mark.SKA_mid
+def test_abort_command(tango_context):
+    logger.info("%s", tango_context)
+    _, abort_command, adapter_factory = get_dishln_command_obj(Abort)
+
+    assert abort_command.check_allowed()
+    (result_code, _) = abort_command.do()
+    assert result_code == ResultCode.OK
+    adapter = adapter_factory.get_or_create_adapter(DISH_MASTER_DEVICE)
+    adapter.proxy.AbortCommands.assert_called_once_with()
+
+
+@pytest.mark.SKA_mid
+def test_abort_command_fail_check_allowed_with_device_unresponsive(
+    tango_context,
+):
+    logger.info("%s", tango_context)
+    cm, abort_command, _ = get_dishln_command_obj(Abort)
+    cm.get_device().update_unresponsive(True)
+    with pytest.raises(
+        DeviceUnresponsive, match=f"{DISH_MASTER_DEVICE} not available"
+    ):
+        abort_command.check_allowed()
