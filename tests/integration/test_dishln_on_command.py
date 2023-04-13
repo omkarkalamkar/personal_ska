451ac78c97a4b0a8aab10f482d48d2307c58dabb
Muskan Sharma <muskans.ska@gmail.com>
2023-04-13 18:31:23 +0530

    HM-184: Implement Integration test cases of On and OFF command.

---
+++ tests/integration/test_dishln_on_command.py
@@ -0,0 +1,70 @@
+import pytest
+import tango
+from ska_tango_base.commands import ResultCode
+from ska_tmc_common.dev_factory import DevFactory
+from ska_tmc_common.enum import DishMode
+
+from tests.settings import (
+    DISH_LEAF_NODE_DEVICE,
+    DISH_MASTER_DEVICE,
+    event_remover,
+    logger,
+)
+
+
+def on_command(tango_context, dishln_name, group_callback):
+    logger.info(f"{tango_context}")
+    dev_factory = DevFactory()
+    dish_leaf_node = dev_factory.get_device(dishln_name)
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    dish_master.SetDirectDishMode(DishMode.STANDBY_LP)
+    dish_master.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["dishMode"],
+    )
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_LP),
+        lookahead=2,
+    )
+    event_remover(
+        group_callback,
+        ["longRunningCommandsInQueue", "longRunningCommandResult"],
+    )
+    dish_leaf_node.subscribe_event(
+        "longRunningCommandsInQueue",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandsInQueue"],
+    )
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        None,
+    )
+
+    result, unique_id = dish_leaf_node.On()
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        ("On",),
+    )
+
+    logger.info(f"Command ID: {unique_id} Returned result: {result}")
+    assert result[0] == ResultCode.QUEUED
+
+    dish_leaf_node.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandResult"],
+    )
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id[0], str(int(ResultCode.OK))),
+        lookahead=2,
+    )
+
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        None,
+        lookahead=2,
+    )
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_on_command(tango_context, group_callback):
+    on_command(tango_context, DISH_LEAF_NODE_DEVICE, group_callback)
