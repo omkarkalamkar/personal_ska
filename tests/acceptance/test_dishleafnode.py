552c66ce70827fe7eabade307ffab8124e87e411
roshant.ska <roshant.ska@gmail.com>
2023-07-26 12:29:15 +0530

    HM-269: Resolve acceptance test falure.

--- tests/acceptance/test_dishleafnode.py
+++ tests/acceptance/test_dishleafnode.py
@@ -127,7 +127,7 @@ def check_command(
 
     group_callback["longRunningCommandsInQueue"].assert_change_event(
         None,
-        lookahead=4,
+        lookahead=6,
     )
     assert str(dish_master_proxy.state()) == resultant_state
 
