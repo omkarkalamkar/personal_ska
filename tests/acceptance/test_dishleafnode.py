eb451fb6ed582a638b51ab8d50b3fd1517a8e42f
roshant.ska <roshant.ska@gmail.com>
2023-07-28 18:58:42 +0530

    HM-269: Check with increased lookahead acceptance test cofigure.

--- tests/acceptance/test_dishleafnode.py
+++ tests/acceptance/test_dishleafnode.py
@@ -127,7 +127,7 @@ def check_command(
 
     group_callback["longRunningCommandsInQueue"].assert_change_event(
         None,
-        lookahead=6,
+        lookahead=10,
     )
     assert str(dish_master_proxy.state()) == resultant_state
 
