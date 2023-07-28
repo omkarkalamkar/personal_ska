8fcfe8a3cc21ab59d1332e199248158a61687d6e
roshant.ska <roshant.ska@gmail.com>
2023-07-28 15:18:31 +0530

    HM-269: Resolve review comments.

--- tests/unit/test_off_command.py
+++ tests/unit/test_off_command.py
@@ -3,7 +3,7 @@ from ska_tango_base.commands import ResultCode, TaskStatus
 from ska_tmc_common.enum import DishMode
 from ska_tmc_common.exceptions import CommandNotAllowed
 
-from tests.settings import create_cm, logger
+from tests.settings import create_cm
 
 
 def test_off_command_in_lp(tango_context, dish_master_device, task_callback):
@@ -32,6 +32,7 @@ def test_off_command_in_fp(tango_context, dish_master_device, task_callback):
     task_callback.assert_against_call(call_kwargs={"status": TaskStatus.IN_PROGRESS})
     task_callback.assert_against_call(status=TaskStatus.COMPLETED, result=ResultCode.OK)
 
+
 @pytest.mark.r1
 def test_off_command_adapter_none(dish_master_device, task_callback):
     cm = create_cm(dish_master_device)
@@ -46,8 +47,10 @@ def test_off_command_adapter_none(dish_master_device, task_callback):
         result=ResultCode.FAILED,
     )
 
-    assert "Failed to connect to database on host tango-databaseds with port 10000" in asserted_data["exception"]
-        
+    assert (
+        "Failed to connect to database on host tango-databaseds with port 10000"
+        in asserted_data["exception"]
+    )
 
 
 def test_off_command_not_allowed(tango_context, dish_master_device):
