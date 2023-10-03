ab25c9d7b65f15467ec0a1c74d419b2d87d6c93f
Roshan Tayde <roshant.ska@gmail.com>
2023-10-03 12:56:17 +0530

    HM-324: Add forward transform.

--- src/ska_tmc_dishleafnode/az_el_converter.py
+++ src/ska_tmc_dishleafnode/az_el_converter.py
@@ -11,11 +11,14 @@ This module defines the AzElConverter class,
 which is used to convert given Ra and Dec values into AzEl."""
 # Standard Python imports
 import logging
+
+from astropy import units as u
+from astropy.coordinates import Angle
 from katpoint import RefractionCorrection, Target
 from ska_tmc_common.dish_utils import DishHelper
+
 logger = logging.getLogger(__name__)
-from astropy.coordinates import Angle
-from astropy import units as u
+
 
 class AzElConverter:
     """Class to convert Right ascension(Ra) and Declination(Dec)
@@ -39,7 +42,7 @@ class AzElConverter:
             if antenna.name == self.component_manager.dish_id:
                 self.component_manager.observer = antenna
 
-    def point(self, right_ascension: str, declination: str, timestamp:str) -> list:
+    def point(self, right_ascension: str, declination: str, timestamp: str) -> list:
         """This method converts Target RaDec coordinates
         to the AzEl coordinates.It is called continuosly
         from Track command (in a thread) at interval
@@ -51,8 +54,10 @@ class AzElConverter:
         Return:
             az_el_coordinates (list)
         """
-        
-        return self.forward_transform(right_ascension=right_ascension,declination=declination,timestamp=timestamp)
+
+        return self.forward_transform(
+            right_ascension=right_ascension, declination=declination, timestamp=timestamp
+        )
 
     def backward_transform(self, az_value, el_value, timestamp) -> list:
         """This method converts given Azimuth/Elevation to RA/Dec after
@@ -70,41 +75,48 @@ class AzElConverter:
         refraction_corrected_el = self.refraction_correction.reverse(el_value, 0.0, 0.0, 0.0)
         elevation_angle = Angle(refraction_corrected_el, u.rad)
         elevation_angle = elevation_angle.dms
-        elevation_angle = f"{int(elevation_angle[0])}:{abs(int(elevation_angle[1]))}:{abs(round(elevation_angle[2],2))}"
-        azimuth_angle = Angle(az_value,u.deg)
+        elevation_angle = f"{int(elevation_angle[0])}\
+            :{abs(int(elevation_angle[1]))}:{abs(round(elevation_angle[2],2))}"
+        azimuth_angle = Angle(az_value, u.deg)
         azimuth_angle = azimuth_angle.dms
-        azimuth_angle = f"{int(azimuth_angle[0])}:{int(azimuth_angle[1])}:{round(azimuth_angle[2],2)}"
+        azimuth_angle = (
+            f"{int(azimuth_angle[0])}:{int(azimuth_angle[1])}:{round(azimuth_angle[2],2)}"
+        )
         target = Target.from_azel(
             azimuth_angle,
             elevation_angle,
-         )
+        )
         ra_dec = target.radec(timestamp=timestamp, antenna=self.component_manager.observer)
         return [ra_dec.ra.deg, ra_dec.dec.deg]
-    
-    def forward_transform(self, right_ascension: str, declination:str, timestamp:str) -> list:
-        """This method invokes the katpoint commands to do the forward transform required 
+
+    def forward_transform(self, right_ascension: str, declination: str, timestamp: str) -> list:
+        """This method invokes the katpoint commands to do the forward transform required
         for pointing a celestial object.
         Forward Transform ie: Geocentric conversion then topocentric and then refraction
         correction.
-        
+
         :param right_ascension: Right Ascension value
         :dtype: string in hours:minutes:seconds form
         :param declination: Declination value.
         :dtype: string in the form of "degree:minutes:seconds"
             dec_value (str): Dec Value in degree:arc_minutes:arc_sec
-        
+
         Return:
             az_el_coordinates (list)
-        """       
-        
-        ra_angle = Angle(right_ascension,u.hourangle)
-        dec_angle = Angle(declination,u.deg)
-        target = Target.from_radec(ra_angle, dec_angle)  # Create KATPoint Target object
-        azel = target.azel(timestamp, self.component_manager.observer) #Does geocentric and topocentric conversions and returns SkyCord object.        
-        elevation_angle = Angle(azel.alt.deg, u.deg)
-        refraction_corrected_el_in_rad = self.refraction_correction.apply(elevation_angle.rad, 0.0, 0.0, 0.0) #Does the refraction corrections(Atmospheric conversion ADR-76)
-        refraction_corrected_angle = Angle(refraction_corrected_el_in_rad,u.rad)
-        return [azel.az.deg, refraction_corrected_angle.deg]   # list of az el co-ordinates in degrees
-    
-    
+        """
 
+        ra_angle = Angle(right_ascension, u.hourangle)
+        dec_angle = Angle(declination, u.deg)
+        target = Target.from_radec(ra_angle, dec_angle)  # Create KATPoint Target object
+        azel = target.azel(
+            timestamp, self.component_manager.observer
+        )  # Does geocentric and topocentric conversions and returns SkyCord object.
+        elevation_angle = Angle(azel.alt.deg, u.deg)
+        refraction_corrected_el_in_rad = self.refraction_correction.apply(
+            elevation_angle.rad, 0.0, 0.0, 0.0
+        )  # Does the refraction corrections(Atmospheric conversion ADR-76)
+        refraction_corrected_angle = Angle(refraction_corrected_el_in_rad, u.rad)
+        return [
+            azel.az.deg,
+            refraction_corrected_angle.deg,
+        ]  # list of az el co-ordinates in degrees
--- tests/unit/test_pointing_calculations.py
+++ tests/unit/test_pointing_calculations.py
@@ -1,82 +1,79 @@
 import logging
 import math
-from katpoint import conversion as cc
+
 import pytest
-from ska_tmc_common.dish_utils import DishHelper
-import re
+from astropy import units as u
+from astropy.coordinates import Angle, SkyCoord
+from ska_tmc_common.exceptions import ConversionError
+
 from ska_tmc_dishleafnode.az_el_converter import AzElConverter
 from ska_tmc_dishleafnode.manager import DishLNComponentManager
-from ska_tmc_common.exceptions import ConversionError
-from astropy.coordinates import Angle
-from astropy import units as u
-from astropy.coordinates import SkyCoord
+
 logger = logging.getLogger(__name__)
 
 
 def degree_to_degree_minute_seconds(argin: float) -> str:
-        """
-        Converts a number in degree decimal to Deg:Min:Sec.
+    """
+    Converts a number in degree decimal to Deg:Min:Sec.
 
-        :param argin: A number in decimal degrees.
-            Example: 30.7129252
-        :return: Number in deg:min:sec format.
-            Example: 30:42:46.5307 is returned value for input 30.7129252.
-        """
-        dms_str = ""  # degree:minutes:seconds
-        try:
-            sign = 1
-            if argin < 0:
-                sign = -1
-            fraction_min, degrees = math.modf(abs(argin))
-            fraction_sec, minutes = math.modf(fraction_min * 60)
-            seconds = fraction_sec * 60
-            dms_str = (
-                f"{int(degrees * sign)}:{int(minutes)}:{round(seconds, 2)}"
-            )
-        except Exception as error:
-            logger.error(
-                "Error while converting decimal degree to deg:min:sec -> %s",
-                error,
-            )
-            raise ConversionError(
-                f"Error while converting {argin} to Degree:Minutes:Seconds"
-            ) from error
-        return str(dms_str)
+    :param argin: A number in decimal degrees.
+        Example: 30.7129252
+    :return: Number in deg:min:sec format.
+        Example: 30:42:46.5307 is returned value for input 30.7129252.
+    """
+    dms_str = ""  # degree:minutes:seconds
+    try:
+        sign = 1
+        if argin < 0:
+            sign = -1
+        fraction_min, degrees = math.modf(abs(argin))
+        fraction_sec, minutes = math.modf(fraction_min * 60)
+        seconds = fraction_sec * 60
+        dms_str = f"{int(degrees * sign)}:{int(minutes)}:{round(seconds, 2)}"
+    except Exception as error:
+        logger.error(
+            "Error while converting decimal degree to deg:min:sec -> %s",
+            error,
+        )
+        raise ConversionError(
+            f"Error while converting {argin} to Degree:Minutes:Seconds"
+        ) from error
+    return str(dms_str)
+
 
 @pytest.mark.parametrize(
-          "ra, dec, timestamp",
-          [
-            pytest.param("16:29:24.46","-26:25:55.7","2019-02-19 06:01:00"),
-            pytest.param("18:36:56.49","38:47:07.8","2019-02-19 05:47:08"),
-            pytest.param("22:50:52.23","18:00:07.7","2019-03-06 07:00:00"),
-
-          ],
+    "ra, dec, timestamp",
+    [
+        pytest.param("16:29:24.46", "-26:25:55.7", "2019-02-19 06:01:00"),
+        pytest.param("18:36:56.49", "38:47:07.8", "2019-02-19 05:47:08"),
+        pytest.param("22:50:52.23", "18:00:07.7", "2019-03-06 07:00:00"),
+    ],
 )
 @pytest.mark.r1
-def test_pointing_calculations(ra:str,dec:str,timestamp:str):
+def test_pointing_calculations(ra: str, dec: str, timestamp: str):
     """Function to test AzEl conversion"""
-      
+
     dish_dev_name = "ska001/dish/master"
     logger = logging.getLogger(__name__)
     component_manager = DishLNComponentManager(logger=logger, dish_dev_name=dish_dev_name)
-    
-    #Below steps does forward transform
+
+    # Below steps does forward transform
     pointing_calc = AzElConverter(component_manager)
     pointing_calc.create_antenna_obj()
     azel = pointing_calc.point(ra, dec, timestamp)
 
-    #Below steps does forward transform
-    refraction_angle = Angle(azel[1],u.deg)
-    ra_dec = pointing_calc.backward_transform(azel[0],refraction_angle.rad,timestamp=timestamp)
+    # Below steps does forward transform
+    refraction_angle = Angle(azel[1], u.deg)
+    ra_dec = pointing_calc.backward_transform(azel[0], refraction_angle.rad, timestamp=timestamp)
 
-    #Below steps will create an skycoord object in icrs frame for the provided Ra, Dec values
-    Ra_angle = Angle(ra,u.hourangle)
-    Dec_angle = Angle(dec,u.deg)
-    given_ra_dec_in_icrs_frame = SkyCoord(Ra_angle, Dec_angle,frame="icrs")
+    # Below steps will create an skycoord object in icrs frame for the provided Ra, Dec values
+    Ra_angle = Angle(ra, u.hourangle)
+    Dec_angle = Angle(dec, u.deg)
+    given_ra_dec_in_icrs_frame = SkyCoord(Ra_angle, Dec_angle, frame="icrs")
 
-    #Below steps does the validation
+    # Below steps does the validation
     converted_ra_angle = Angle(ra_dec[0], u.deg)
-    converted_dec_angle = Angle(ra_dec[1],u.deg)
+    converted_dec_angle = Angle(ra_dec[1], u.deg)
     converted_ra_dec = SkyCoord(converted_ra_angle, converted_dec_angle, frame="icrs")
     given_ra = given_ra_dec_in_icrs_frame.ra.to_string(u.rad, precision=5)
     given_dec = given_ra_dec_in_icrs_frame.dec.to_string(u.rad, precision=4)
@@ -84,16 +81,3 @@ def test_pointing_calculations(ra:str,dec:str,timestamp:str):
     converted_dec = converted_ra_dec.dec.to_string(u.rad, precision=4)
     assert given_ra == converted_ra
     assert given_dec == converted_dec
-    # ra1Angle= Angle(ra_dec[0],u.deg)
-    # ra1_hms = round(ra1Angle.hourangle,3)
-    # ra2Angle = Angle(ra,u.hourangle)
-    # ra2_hms = round(ra2Angle.hourangle,3)
-    # assert ra1_hms == ra2_hms
-    # dec_angle1 = round(ra_dec[1],3)
-    # dec_angle2 = round(Angle(dec, u.deg).deg,3)
-    # assert dec_angle1 == dec_angle2
-    # dec_angle2 = Angle(dec,u.deg)
-    # dec_val = dec_angle1.arcsec
-    # dec_val2 = dec_angle2.arcsec
-    # #assert ra_val == ra
-    # assert dec_val== dec_val2
