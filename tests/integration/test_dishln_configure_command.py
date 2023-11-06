ef3aa1a059c4a083da678475e32cab3db7573622
Bhushan Parakh <bhushan.ska@gmail.com>
2023-11-06 15:42:03 +0530

    HM-361: Adding test cases

--- tests/integration/test_dishln_configure_command.py
+++ tests/integration/test_dishln_configure_command.py
@@ -108,3 +108,119 @@ def test_configure_command(tango_context, group_callback, json_factory):
         group_callback,
         json_factory("dishleafnode_configure"),
     )
+
+
+def partial_configure_dish_leaf_node(
+    tango_context,
+    dishln_name,
+    group_callback,
+    configure_input_str,
+    partial_configure_input_str,
+):
+    """Partial configure flow for dish leaf node."""
+    dev_factory = DevFactory()
+    dish_leaf_node = dev_factory.get_device(dishln_name)
+    event_remover(
+        group_callback,
+        ["longRunningCommandsInQueue", "longRunningCommandResult"],
+    )
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    dish_master.SetDirectDishMode(DishMode.STANDBY_LP)
+    dish_master.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["dishMode"],
+    )
+    dish_master.subscribe_event(
+        "pointingState",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["pointingState"],
+    )
+
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_LP),
+        lookahead=2,
+    )
+
+    dish_leaf_node.subscribe_event(
+        "longRunningCommandsInQueue",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandsInQueue"],
+    )
+
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        None,
+    )
+
+    result_fp, unique_id_fp = dish_leaf_node.SetStandbyFPMode()
+    assert result_fp[0] == ResultCode.QUEUED
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        ("SetStandbyFPMode",),
+        lookahead=2,
+    )
+    dish_leaf_node.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandResult"],
+    )
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_fp[0], str(int(ResultCode.OK))),
+        lookahead=2,
+    )
+
+    result_config, unique_id_config = dish_leaf_node.Configure(configure_input_str)
+    assert result_config[0] == ResultCode.QUEUED
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        ("SetStandbyFPMode", "Configure")
+    )
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_config[0], str(int(ResultCode.OK))),
+        lookahead=6,
+    )
+
+    result_config, unique_id_config = dish_leaf_node.Configure(partial_configure_input_str)
+    assert result_config[0] == ResultCode.QUEUED
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        ("Configure", "Configure"), lookahead=3
+    )
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_config[0], str(int(ResultCode.OK))),
+        lookahead=6,
+    )
+
+    result_config, unique_id_config = dish_leaf_node.TrackStop()
+
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        ("TrackStop",),
+        lookahead=6,
+    )
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_config[0], str(int(ResultCode.OK))),
+        lookahead=6,
+    )
+
+    group_callback["pointingState"].assert_change_event(
+        (PointingState.READY),
+        lookahead=6,
+    )
+
+    group_callback["longRunningCommandsInQueue"].assert_change_event(
+        None,
+        lookahead=8,
+    )
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_partial_configure_command(tango_context, group_callback, json_factory):
+    """Test partial configure functionality on Dish Leaf Node."""
+    partial_configure_dish_leaf_node(
+        tango_context,
+        DISH_LEAF_NODE_DEVICE,
+        group_callback,
+        json_factory("dishleafnode_configure"),
+        json_factory("partial_configure"),
+    )
--- tests/unit/test_configure_command.py
+++ tests/unit/test_configure_command.py
@@ -1,9 +1,11 @@
+import json
+
 import pytest
 from ska_tango_base.commands import ResultCode, TaskStatus
 from ska_tmc_common.enum import DishMode
 from ska_tmc_common.exceptions import CommandNotAllowed
 
-from tests.settings import create_cm, logger, wait_for_dish_mode
+from tests.settings import create_cm, wait_for_dish_mode
 
 
 def test_configure_command_completed(
@@ -32,20 +34,37 @@ def test_configure_command_completed(
 def test_configure_command_completed_partial_config(
     tango_context, task_callback, dish_master_device, json_factory
 ):
+    """Test partial configure functionality"""
     cm = create_cm(dish_master_device)
-    cm.update_device_dish_mode(DishMode.STANDBY_LP)
-    cm.is_setstandbyfpmode_allowed()
-    cm.setstandbyfpmode(task_callback)
+    cm.update_device_dish_mode(DishMode.OPERATE)
+
+    assert cm.is_configure_allowed()
+    configure_input_str = json_factory("partial_configure")
+
+    cm.configure(configure_input_str, task_callback=task_callback)
+
     task_callback.assert_against_call(call_kwargs={"status": TaskStatus.QUEUED})
     task_callback.assert_against_call(call_kwargs={"status": TaskStatus.IN_PROGRESS})
     task_callback.assert_against_call(
         call_kwargs={"status": TaskStatus.COMPLETED, "result": ResultCode.OK}
     )
-    assert wait_for_dish_mode(cm, DishMode.STANDBY_FP)
+
+
+def test_configure_command_completed_partial_config_missing_key(
+    tango_context, task_callback, dish_master_device, json_factory
+):
+    """Test partial configure functionality"""
+    cm = create_cm(dish_master_device)
+    cm.update_device_dish_mode(DishMode.OPERATE)
+
     assert cm.is_configure_allowed()
     configure_input_str = json_factory("partial_configure")
-    logger.info("The input data string is -> %s", configure_input_str)
+    config_json = json.loads(configure_input_str)
+    del config_json["pointing"]["target"]["ca_offset_arcsec"]
+    configure_input_str = json.dumps(config_json)
+
     cm.configure(configure_input_str, task_callback=task_callback)
+
     task_callback.assert_against_call(call_kwargs={"status": TaskStatus.QUEUED})
     task_callback.assert_against_call(call_kwargs={"status": TaskStatus.IN_PROGRESS})
     task_callback.assert_against_call(
@@ -65,14 +84,18 @@ def test_configure_command_adapter_none(task_callback, dish_master_device, json_
     task_callback.assert_against_call(status=TaskStatus.COMPLETED, result=ResultCode.FAILED)
 
 
-def test_json_validation(tango_context, task_callback, dish_master_device, json_factory):
+@pytest.mark.parametrize("key", ["pointing", "dish"])
+def test_json_validation(tango_context, task_callback, dish_master_device, json_factory, key):
     cm = create_cm(dish_master_device)
     cm.update_device_dish_mode(DishMode.STANDBY_FP)
     assert cm.is_configure_allowed()
-    configure_input_str = json_factory("invalid_key")
+    configure_input_str = json_factory("dishleafnode_configure")
+    config_json = json.loads(configure_input_str)
+    del config_json[key]
+    configure_input_str = json.dumps(config_json)
     result, message = cm.configure(configure_input_str, task_callback=task_callback)
     assert result == ResultCode.FAILED
-    assert "key is not present" in message
+    assert f"{key} key is not present" in message
 
 
 def test_configure_command_not_allowed(tango_context, dish_master_device):
