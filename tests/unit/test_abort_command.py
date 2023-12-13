e70d133bb71ca03e6caa7f62df49ee8e2f3f2cf1
Roshan Tayde <roshant.ska@gmail.com>
2023-12-13 17:59:45 +0530

    HM-349: Resolve device unresponsive test case issue.

--- tests/unit/test_abort_command.py
+++ tests/unit/test_abort_command.py
@@ -3,7 +3,7 @@ from ska_tango_base.commands import ResultCode
 from ska_tmc_common.exceptions import DeviceUnresponsive
 
 from ska_tmc_dishleafnode.commands.abort_command import AbortCommands
-from tests.settings import DISH_MASTER_DEVICE, create_cm, logger, wait_for_unresponsive
+from tests.settings import DISH_MASTER_DEVICE, create_cm, logger
 
 
 def test_abort_command(tango_context):
@@ -14,12 +14,15 @@ def test_abort_command(tango_context):
     assert result_code == ResultCode.OK
 
 
+@pytest.mark.test
 def test_abort_command_fail_check_allowed_with_device_unresponsive(
     tango_context,
 ):
     logger.info("%s", tango_context)
     cm = create_cm(DISH_MASTER_DEVICE)
     cm.get_device().update_unresponsive(True)
-    wait_for_unresponsive(cm)
-    with pytest.raises(DeviceUnresponsive, match=f"{DISH_MASTER_DEVICE} not available"):
-        cm.is_abortcommands_allowed()
+    if cm.get_device().unresponsive:
+        with pytest.raises(DeviceUnresponsive, match=f"{DISH_MASTER_DEVICE} not available"):
+            cm.is_abortcommands_allowed()
+    else:
+        raise AssertionError("Failed to check DeviceUnresponsive with abort command.")
