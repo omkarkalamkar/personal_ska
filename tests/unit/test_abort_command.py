948b02eef1359d10d888439efc5493b6cd6f225c
Roshan Tayde <roshant.ska@gmail.com>
2023-12-14 09:24:35 +0530

    HM-349: Resolve unresponsive unit test case issue

--- tests/unit/test_abort_command.py
+++ tests/unit/test_abort_command.py
@@ -3,7 +3,7 @@ from ska_tango_base.commands import ResultCode
 from ska_tmc_common.exceptions import DeviceUnresponsive
 
 from ska_tmc_dishleafnode.commands.abort_command import AbortCommands
-from tests.settings import DISH_MASTER_DEVICE, create_cm, logger
+from tests.settings import DISH_MASTER_DEVICE, create_cm, logger, wait_for_unresponsive
 
 
 def test_abort_command(tango_context):
@@ -18,10 +18,10 @@ def test_abort_command(tango_context):
 def test_abort_command_fail_check_allowed_with_device_unresponsive(
     tango_context,
 ):
-    logger.info("%s", tango_context)
     cm = create_cm(DISH_MASTER_DEVICE)
     cm.get_device().update_unresponsive(True)
-    if cm.get_device().unresponsive:
+    result = wait_for_unresponsive(cm)
+    if result:
         with pytest.raises(DeviceUnresponsive, match=f"{DISH_MASTER_DEVICE} not available"):
             cm.is_abortcommands_allowed()
     else:
