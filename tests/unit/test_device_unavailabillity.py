5074b0e936794d91393edbb0d75f4989db0c38f8
devesh.ska <devesh.ska@gmail.com>
2023-12-28 17:34:00 +0530

    HM-391:try updating test

--- tests/unit/test_device_unavailabillity.py
+++ tests/unit/test_device_unavailabillity.py
@@ -5,21 +5,5 @@ from ska_tmc_dishleafnode.dish_leaf_node import DishLeafNode
 from tests.settings import DISH_LEAF_NODE_DEVICE, logger
 
 
-@pytest.fixture()
-def devices_to_load():
-    """Returns helper state devices."""
-    return (
-        {
-            "class": DishLeafNode,
-            "devices": [
-                {"name": "ska_mid/tm_leaf_node/d0001"},
-            ],
-        },
-    )
-
-
-def test_check_device_availabillity(tango_context):
-    logger.info(f"{tango_context}")
-    dev_factory = DevFactory()
-    dish_leaf_node = dev_factory.get_device(DISH_LEAF_NODE_DEVICE)
-    assert dish_leaf_node.isSubsystemAvailable
+def test_check_device_availabillity(dishln_device):
+    assert dishln_device.isSubsystemAvailable
