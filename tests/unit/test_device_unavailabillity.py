fbcb2de1ab240c992a762b3882e4a0d482d6baa9
devesh.ska <devesh.ska@gmail.com>
2023-12-28 17:35:33 +0530

    HM-391:try updating test

--- tests/unit/test_device_unavailabillity.py
+++ tests/unit/test_device_unavailabillity.py
@@ -1,9 +1,2 @@
-import pytest
-from ska_tmc_common import DevFactory
-
-from ska_tmc_dishleafnode.dish_leaf_node import DishLeafNode
-from tests.settings import DISH_LEAF_NODE_DEVICE, logger
-
-
 def test_check_device_availabillity(dishln_device):
     assert dishln_device.isSubsystemAvailable
