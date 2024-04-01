305b3a980a4352045bfb54daa5f6cc2cb6a4218e
Mansi Shirore <mansis.ska@gmail.com>
2024-04-01 18:49:53 +0530

    SAH-1498: Updated test cases

--- tests/unit/test_off_command.py
+++ tests/unit/test_off_command.py
@@ -1,11 +1,7 @@
 import pytest
-import tango
 from ska_tango_base.commands import ResultCode, TaskStatus
-from ska_tmc_common import DevFactory
 from ska_tmc_common.enum import DishMode
 from ska_tmc_common.exceptions import CommandNotAllowed
-
-from tests.settings import wait_for_dish_mode
 
 
 def test_off_command_in_lp(tango_context, cm):
@@ -14,22 +10,8 @@ def test_off_command_in_lp(tango_context, cm):
         cm.is_off_allowed()
 
 
-def test_off_command_in_fp(tango_context, cm, task_callback, group_callback):
-    dish_device = DevFactory().get_device("ska001/elt/master")
-    dish_device.subscribe_event(
-        "dishMode",
-        tango.EventType.CHANGE_EVENT,
-        group_callback["dishMode"],
-        stateless=True,
-    )
-    cm.setstandbyfpmode(task_callback)
-    task_callback.assert_against_call(call_kwargs={"status": TaskStatus.QUEUED})
-    task_callback.assert_against_call(call_kwargs={"status": TaskStatus.IN_PROGRESS})
-    task_callback.assert_against_call(
-        call_kwargs={"status": TaskStatus.COMPLETED, "result": ResultCode.OK}
-    )
-    group_callback["dishMode"].assert_change_event(DishMode.STANDBY_FP, lookahead=4)
-    assert wait_for_dish_mode(cm, DishMode.STANDBY_FP)
+def test_off_command_in_fp(tango_context, cm, task_callback):
+    cm.update_device_dish_mode(DishMode.STANDBY_FP)
     assert cm.is_off_allowed()
 
     cm.off(task_callback=task_callback)
