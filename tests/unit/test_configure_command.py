bd61c173c19b16c2909a0a4d7c325e1f55eb624e
JayantKumbhar <jayant.ska@gmail.com>
2024-04-03 15:14:57 +0530

    SAH-1472: Revert unit test related changes

--- tests/unit/test_configure_command.py
+++ tests/unit/test_configure_command.py
@@ -17,7 +17,14 @@ def test_configure_command_completed(
     task_callback,
     json_factory,
 ):
-    cm.update_device_dish_mode(DishMode.STANDBY_FP)
+    cm.update_device_dish_mode(DishMode.STANDBY_LP)
+    cm.is_setstandbyfpmode_allowed()
+    cm.setstandbyfpmode(task_callback)
+    task_callback.assert_against_call(call_kwargs={"status": TaskStatus.QUEUED})
+    task_callback.assert_against_call(call_kwargs={"status": TaskStatus.IN_PROGRESS})
+    task_callback.assert_against_call(
+        call_kwargs={"status": TaskStatus.COMPLETED, "result": ResultCode.OK}
+    )
     assert wait_for_dish_mode(cm, DishMode.STANDBY_FP)
     assert cm.is_configure_allowed()
     configure_input_str = json_factory("dishleafnode_configure")
