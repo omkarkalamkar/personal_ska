5061fe56c8339448d83161a4e00267c5b0d72ae2
SKAIN <truptin.ska@gmail.com>
2024-05-02 08:55:03 +0530

    SAH-1530: updated  test file

--- tests/unit/test_track_command.py
+++ tests/unit/test_track_command.py
@@ -30,7 +30,10 @@ def test_track_command_completed(tango_context, task_callback, cm):
         call_kwargs={"status": TaskStatus.IN_PROGRESS}
     )
     task_callback.assert_against_call(
-        call_kwargs={"status": TaskStatus.COMPLETED, "result": ResultCode.OK}
+        call_kwargs={
+            "status": TaskStatus.COMPLETED,
+            "result": ResultCode.QUEUED,
+        }
     )
 
 
