afa0555fc288e4b9bded59cd86d0fdfc80ed589e
apurvapatkar <apurva.ska@gmail.com>
2024-07-02 09:08:15 +0530

    SAH-1518: Execute tests.

--- tests/integration/test_sdpqc_functionality.py
+++ tests/integration/test_sdpqc_functionality.py
@@ -39,9 +39,6 @@ def utc_timestamp(timestamp: str) -> float:
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
 def test_sdpqc_functionality(tango_context, group_callback):
-    # Wait added for the previous Program Track Table Thread to stop
-    time.sleep(2)
-
     sdp_queue_connector = DevFactory().get_device(SDP_QUEUE_CONNECTOR_DEVICE)
     dish_leaf_node = DevFactory().get_device(DISH_LEAF_NODE_DEVICE)
     dish_master = DevFactory().get_device(DISH_MASTER_DEVICE)
@@ -91,7 +88,7 @@ def test_sdpqc_functionality(tango_context, group_callback):
             flag = False
             break
         elapsed_time = elapsed_time + 1
-        time.sleep(1)
+        time.sleep(0.01)
     assert not flag
     received_actual_pointing = json.loads(dish_leaf_node.actualpointing)
     assert TIMESTAMP_RA_DEC[0] == received_actual_pointing[0]
