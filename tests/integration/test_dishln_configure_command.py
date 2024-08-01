3c9bc1582297820b7f47cab64b9c030c3cfc42f1
manthan <manthan.ska@gmail.com>
2024-08-01 17:02:31 +0530

    SAH-1530: Test mark test case only

--- tests/integration/test_dishln_configure_command.py
+++ tests/integration/test_dishln_configure_command.py
@@ -379,6 +379,7 @@ def test_configure_command(tango_context, group_callback, json_factory):
     )
 
 
+@pytest.mark.MM
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
 def test_unhappy_configure_command(
