e7b49b7afb49cb1e100cde965bc7f1c8780ec750
Jatin Sharma <jatins.ska@gmail.com>
2024-08-02 09:00:32 +0530

    HM-509:Adding test case for partial configure correction key

--- tests/integration/test_dishln_configure_correction_key.py
+++ tests/integration/test_dishln_configure_correction_key.py
@@ -1,3 +1,4 @@
+import json
 from time import sleep
 
 import pytest
@@ -109,4 +110,113 @@ def test_pointing_correction_key(tango_context, json_factory, group_callback):
     dish_leaf_node.unsubscribe_event(POINTINGSTATE_ID)
     dish_leaf_node.unsubscribe_event(LRCR_ID)
 
+    tear_down(dish_leaf_node, dish_master, group_callback)
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_partial_configure_dish_leaf_node_correction_key(
+    tango_context,
+    group_callback,
+    json_factory,
+):
+    """Partial configure flow for dish leaf node."""
+    dev_factory = DevFactory()
+    dish_leaf_node = dev_factory.get_device(DISH_LEAF_NODE_DEVICE)
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    dish_master.SetDirectDishMode(DishMode.STANDBY_LP)
+    sleep(1)
+    DISHMODE_ID = dish_leaf_node.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["dishMode"],
+    )
+    POINTINGSTATE_ID = dish_leaf_node.subscribe_event(
+        "pointingState",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["pointingState"],
+    )
+
+    SOURCE_OFFSET_ID = dish_leaf_node.subscribe_event(
+        "sourceOffset",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["sourceOffset"],
+    )
+
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_LP),
+        lookahead=2,
+    )
+
+    result_fp, unique_id_fp = dish_leaf_node.SetStandbyFPMode()
+    sleep(1)
+    assert result_fp[0] == ResultCode.QUEUED
+
+    LRCR_ID = dish_leaf_node.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandResult"],
+    )
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_fp[0], COMMAND_COMPLETED),
+        lookahead=2,
+    )
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_FP),
+        lookahead=6,
+    )
+    configure_input_str = json_factory("dishleafnode_configure")
+
+    result_config, unique_id_config = dish_leaf_node.Configure(
+        configure_input_str
+    )
+    assert result_config[0] == ResultCode.QUEUED
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_config[0], COMMAND_COMPLETED),
+        lookahead=6,
+    )
+    partial_configure_input_str = json_factory("partial_configure")
+    load_conf = json.loads(partial_configure_input_str)
+    load_conf["pointing"]["correction"] = "RESET"
+    partial_configure_input_str = json.dumps(load_conf)
+
+    result_config, unique_id_config = dish_leaf_node.Configure(
+        partial_configure_input_str
+    )
+    assert result_config[0] == ResultCode.QUEUED
+    load_conf = json.loads(partial_configure_input_str)
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_config[0], COMMAND_COMPLETED),
+        lookahead=8,
+    )
+    # Assert change event is occuring and values are reflecting
+    # on sourceOffset attribute.
+    group_callback["sourceOffset"].assert_change_event(
+        [0.0, 0.0],
+        lookahead=2,
+    )
+
+    result_trackstop, unique_id_trackstop = dish_leaf_node.TrackStop()
+    assert result_trackstop[0] == ResultCode.QUEUED
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_trackstop[0], COMMAND_COMPLETED),
+        lookahead=6,
+    )
+
+    group_callback["pointingState"].assert_change_event(
+        (PointingState.READY),
+        lookahead=6,
+    )
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.OPERATE),
+        lookahead=6,
+    )
+
+    dish_leaf_node.unsubscribe_event(SOURCE_OFFSET_ID)
+    dish_leaf_node.unsubscribe_event(DISHMODE_ID)
+    dish_leaf_node.unsubscribe_event(POINTINGSTATE_ID)
+    dish_leaf_node.unsubscribe_event(LRCR_ID)
     tear_down(dish_leaf_node, dish_master, group_callback)
