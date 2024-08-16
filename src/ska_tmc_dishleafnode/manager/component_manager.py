de3f36a8143d2cf2ee6870713e3d9fe3b4b99ab5
Akshay Sonawane <akshayss.ska@gmail.com>
2024-08-16 15:16:04 +0200

    SKB-502: update programtaracktable

--- src/ska_tmc_dishleafnode/manager/component_manager.py
+++ src/ska_tmc_dishleafnode/manager/component_manager.py
@@ -1392,7 +1392,10 @@ class DishLNComponentManager(TmcLeafNodeComponentManager):
         :rtype: None
         """
         with self.tango_operation_execution_lock:
-            self.dish_adapter.programTrackTable = program_track_table
+            try:
+                self.dish_adapter.programTrackTable = program_track_table
+            except Exception as exception:
+                self.logger.error(exception)
         self.logger.debug("ProgramTrackTable: %s", program_track_table)
 
     def track_process(
