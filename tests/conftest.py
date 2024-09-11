65201a24c785d467991b8973d6df63d143937701
Akshay Sonawane <akshayss.ska@gmail.com>
2024-09-11 15:38:24 +0530

    SKB-502: update the coftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -107,7 +107,7 @@ def tango_context(devices_to_load, request):
     logging.info("true context: %s", true_context)
     if not true_context:
         with MultiDeviceTestContext(
-            devices_to_load, process=False, timeout=80
+            devices_to_load, process=True, timeout=80
         ) as context:
             DevFactory._test_context = context
             logging.info("test context set")
@@ -129,6 +129,7 @@ def dishln_device(request):
                 "DishAvailabilityCheckTimeout": 10,
             },
             timeout=20,
+            process=True,
         ) as proxy:
             yield proxy
     else:
