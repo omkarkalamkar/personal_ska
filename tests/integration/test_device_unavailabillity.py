5962356e774ac1b60e63fa28b930bcfa131a67cf
devesh.ska <devesh.ska@gmail.com>
2024-09-23 19:23:58 +0530

    HM-537: Update test

--- tests/integration/test_device_unavailabillity.py
+++ tests/integration/test_device_unavailabillity.py
@@ -1,8 +1,11 @@
+import pytest
 from ska_tmc_common import DevFactory
 
 from tests.settings import DISH_LEAF_NODE_DEVICE
 
 
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
 def test_check_device_availabillity():
     dishln_device = DevFactory().get_device(DISH_LEAF_NODE_DEVICE)
     assert dishln_device.isSubsystemAvailable
