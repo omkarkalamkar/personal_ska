c11b8bbff930c4c9038229caa60cb5bf2d7a9c15
JayantKumbhar <jayant.ska@gmail.com>
2024-09-23 14:55:58 +0530

    SAH-1587: Add test case for Abort while configuring scenario

--- tests/integration/test_dishln_abort_command.py
+++ tests/integration/test_dishln_abort_command.py
@@ -101,7 +101,88 @@ def abort_when_configured(
         lookahead=6,
     )
     group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_FP),
+        lookahead=6,
+    )
+    dish_leaf_node.unsubscribe_event(DISHMODE_ID)
+    dish_leaf_node.unsubscribe_event(POINTINGSTATE_ID)
+    dish_leaf_node.unsubscribe_event(LRCR_ID)
+
+
+def abort_while_configuring(
+    tango_context,
+    dishln_name,
+    group_callback,
+    configure_input_str,
+):
+    logger.info(f"{tango_context}")
+    dev_factory = DevFactory()
+    dish_leaf_node = dev_factory.get_device(dishln_name)
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    dish_master.SetDirectDishMode(DishMode.STANDBY_LP)
+    DISHMODE_ID = dish_leaf_node.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["dishMode"],
+    )
+    POINTINGSTATE_ID = dish_leaf_node.subscribe_event(
+        "pointingState",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["pointingState"],
+    )
+
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_LP),
+        lookahead=2,
+    )
+
+    result_fp, unique_id_fp = dish_leaf_node.SetStandbyFPMode()
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_FP),
+        lookahead=6,
+    )
+
+    assert result_fp[0] == ResultCode.QUEUED
+    LRCR_ID = dish_leaf_node.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandResult"],
+    )
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_fp[0], COMMAND_COMPLETED),
+        lookahead=2,
+    )
+
+    result_config, unique_id_config = dish_leaf_node.Configure(
+        configure_input_str
+    )
+    assert result_config[0] == ResultCode.QUEUED
+    logger.info(
+        f"Command ID: {unique_id_config} Returned result: {result_config}"
+    )
+    group_callback["pointingState"].assert_change_event(
+        (PointingState.READY),
+        lookahead=6,
+    )
+    group_callback["dishMode"].assert_change_event(
         (DishMode.OPERATE),
+        lookahead=6,
+    )
+
+    result_abort, unique_id_abort = dish_leaf_node.AbortCommands()
+    logger.info(
+        f"Command ID: {unique_id_abort} Returned result: {result_abort}"
+    )
+
+    assert result_abort == ResultCode.STARTED
+
+    group_callback["pointingState"].assert_change_event(
+        (PointingState.READY),
+        lookahead=6,
+    )
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_FP),
         lookahead=6,
     )
     dish_leaf_node.unsubscribe_event(DISHMODE_ID)
@@ -122,6 +203,17 @@ def test_abort_command(tango_context, group_callback):
 @pytest.mark.SKA_mid
 def test_abort_when_configure(tango_context, group_callback, json_factory):
     abort_when_configured(
+        tango_context,
+        DISH_LEAF_NODE_DEVICE,
+        group_callback,
+        json_factory("dishleafnode_configure"),
+    )
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_abort_while_configuring(tango_context, group_callback, json_factory):
+    abort_while_configuring(
         tango_context,
         DISH_LEAF_NODE_DEVICE,
         group_callback,
