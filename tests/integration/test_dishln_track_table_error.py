c2ac071a04fd2815ae6d67c674d012ec5abf366a
yogeshk.ska <yogeshk.ska@gmail.com>
2024-11-13 20:19:19 +0530

    HM-567: test case fix

--- tests/integration/test_dishln_track_table_error.py
+++ tests/integration/test_dishln_track_table_error.py
@@ -186,18 +186,13 @@ def configure_dish_leaf_node_unknown_source(
     monitor_track_table_errors_attribute(
         dish_leaf_node, track_table_error_before_configure
     )
-    expected_message = (
-        "Exception occurred while starting programTrackTable calculation: "
-        + "Target description 'Pluto, special' contains unknown *special* "
-        + "body 'Pluto'",
-    )
 
     expected_message = (
         "Exception occurred while calculating track table: Exception occurred "
         + "while starting programTrackTable calculation: Target description "
-        + "'Pluto, special' contains unknown *special* body 'Pluto'",
+        + "'Pluto, special' contains unknown *special* body 'Pluto'"
     )
-    assert dish_leaf_node.trackTableErrors == expected_message
+    assert expected_message in dish_leaf_node.trackTableErrors
 
     group_callback["longRunningCommandResult"].assert_change_event(
         (
