0144ed2479070be4213e87a1d0b186c01ab4b678
apurvapatkar <apurva.ska@gmail.com>
2024-11-15 12:26:08 +0530

    SAH-1623: Execute tests.

--- tests/unit/test_track_load_static_off_command.py
+++ tests/unit/test_track_load_static_off_command.py
@@ -163,7 +163,7 @@ def test_configure_command_completed_with_correction_key_reset(
 
 @pytest.mark.skip(reason="Getting TRANSIENT_NoUsableProfile error.")
 def test_configure_command_completed_with_correction_key_update(
-    tango_context_process_true,
+    tango_context,
     cm,
     group_callback,
     task_callback,
