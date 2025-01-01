0315ea9c7df5378d9ccd09d4c04bae1cca87e2ba
yogeshk.ska <yogeshk.ska@gmail.com>
2025-01-01 15:05:18 +0530

    HM-564: 50 times count removed

--- tests/integration/test_dishln_abort_command.py
+++ tests/integration/test_dishln_abort_command.py
@@ -222,7 +222,6 @@ def test_abort_after_configure(tango_context, group_callback, json_factory):
 
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
-@pytest.mark.repeat_test
 def test_abort_while_configuring(tango_context, group_callback, json_factory):
     abort_while_configuring(
         tango_context,
