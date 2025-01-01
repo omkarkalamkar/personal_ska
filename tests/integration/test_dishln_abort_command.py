18df1598020f7ba424a5c6c32a39f487ed30c8e9
yogeshk.ska <yogeshk.ska@gmail.com>
2025-01-01 11:36:59 +0530

    HM-564: count added for test case

--- tests/integration/test_dishln_abort_command.py
+++ tests/integration/test_dishln_abort_command.py
@@ -221,6 +221,8 @@ def test_abort_after_configure(tango_context, group_callback, json_factory):
 
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
+@pytest.mark.repeat_test
+@pytest.mark.repeat(3)
 def test_abort_while_configuring(tango_context, group_callback, json_factory):
     abort_while_configuring(
         tango_context,
