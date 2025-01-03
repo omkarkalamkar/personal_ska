c02b0b0dc24b019bc1d0582fd577bd0016789877
Muskan Sharma <muskans.ska@gmail.com>
2025-01-03 12:57:15 +0530

    HM-587: fix skb-690.

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,7 +81,7 @@ def devices_to_load():
             "class": DishLeafNode,
             "devices": [
                 {
-                    "name": "ska_mid/tm_leaf_node/d0001",
+                    "name": "mid-tmc/leaf-node-dish/SKA001",
                     "properties": {
                         "DishMasterFQDN": DISH_MASTER_DEVICE,
                         "DishlnPointingDeviceFQDN": DISHLN_POINTING_DEVICE,
@@ -142,7 +142,7 @@ def dishln_device(request):
     if not true_context:
         with DeviceTestContext(
             DishLeafNode,
-            device_name="ska_mid/tm_leaf_node/d0001",
+            device_name="mid-tmc/leaf-node-dish/SKA001",
             properties={
                 "DishMasterFQDN": DISH_MASTER_DEVICE,
                 "DishlnPointingDeviceFQDN": DISHLN_POINTING_DEVICE,
--- tests/settings.py
+++ tests/settings.py
@@ -26,7 +26,7 @@ TIMEOUT = 100
 NUMBER_OF_PROGRAM_TRACK_TABLE_ENTRIES = 150
 
 DISH_MASTER_DEVICE = "mid-dish/dish-manager/SKA001"
-DISH_LEAF_NODE_DEVICE = "ska_mid/tm_leaf_node/d0001"
+DISH_LEAF_NODE_DEVICE = "mid-tmc/leaf-node-dish/SKA001"
 SDP_QUEUE_CONNECTOR_DEVICE = "mid-sdp/queueconnector/01"
 SDP_QUEUE_CONNECTOR_DEVICE2 = "mid-sdp/queueconnector/02"
 DISHLN_POINTING_DEVICE = "mid-tmc/dish-pointing/d0001"
