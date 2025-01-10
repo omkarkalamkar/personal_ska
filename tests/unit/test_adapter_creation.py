9df239424e0b386a6d5ca3ff4e359569f4706a07
apurvapatkar <apurva.ska@gmail.com>
2025-01-10 17:20:27 +0530

    SAH-1652: Execute tests.

--- tests/unit/test_adapter_creation.py
+++ tests/unit/test_adapter_creation.py
@@ -1,17 +1,11 @@
 import pytest
 from ska_tango_base.commands import ResultCode
 
-from ska_tmc_dishleafnode.commands.setoperatemode import SetOperateMode
-
 
 def test_adapter_creation_success(tango_context, cm_without_er_lp) -> None:
     cm = cm_without_er_lp
-    setoperatemode_command_obj = SetOperateMode(
-        cm,
-        cm.op_state_model,
-        cm.adapter_factory,
-    )
-    result_code, message = setoperatemode_command_obj.init_adapter()
+    result_code, message = cm.init_adapter()
+
     assert result_code == ResultCode.OK
     assert message == "Adapter initialisation is successful"
 
@@ -19,12 +13,8 @@ def test_adapter_creation_success(tango_context, cm_without_er_lp) -> None:
 @pytest.mark.mccsmln
 def test_adapter_creation_timeout(tango_context, cm_without_er_lp) -> None:
     cm = cm_without_er_lp
-    setoperatemode_command_obj = SetOperateMode(
-        cm,
-        cm.op_state_model,
-        cm.adapter_factory,
-    )
     cm.dish_dev_name = ""
-    result_code, message = setoperatemode_command_obj.init_adapter()
+    result_code, message = cm.init_adapter()
+
     assert result_code == ResultCode.FAILED
     assert "Error in creating adapter for" in message
