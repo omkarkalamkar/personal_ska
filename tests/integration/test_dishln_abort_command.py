0bdf71e1da88a26850fb6dd094f855e449da0af7
JayantKumbhar <jayant.ska@gmail.com>
2025-04-18 09:45:47 +0530

    SAH-1697: Implement test cases for Abort exception handling and timeout

--- tests/integration/test_dishln_abort_command.py
+++ tests/integration/test_dishln_abort_command.py
@@ -1,13 +1,16 @@
+import json
 import time
 
 import pytest
 import tango
 from ska_tango_base.commands import ResultCode
 from ska_tmc_common.dev_factory import DevFactory
-from ska_tmc_common.enum import DishMode, PointingState
+from ska_tmc_common.enum import DishMode, FaultType, PointingState
 
 from tests.settings import (
     COMMAND_COMPLETED,
+    COMMAND_FAILED,
+    COMMAND_TIMEOUT,
     DISH_LEAF_NODE_DEVICE,
     DISH_MASTER_DEVICE,
     TIMEOUT,
@@ -259,3 +262,137 @@ def is_configure_aborted(dish_leaf_node, unique_id_config) -> bool:
         time.sleep(0.1)
         elapsed_time = time.time() - start_time
     return False
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_abort_timeout(tango_context, group_callback):
+    abort_timeout(tango_context, DISH_LEAF_NODE_DEVICE, group_callback)
+
+
+def abort_timeout(
+    tango_context,
+    dishln_name,
+    group_callback,
+):
+    dev_factory = DevFactory()
+    dish_leaf_node = dev_factory.get_device(dishln_name)
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    dish_master.SetDirectDishMode(DishMode.STANDBY_LP)
+    dishmode_event_id = dish_leaf_node.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["dishMode"],
+    )
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_LP),
+        lookahead=2,
+    )
+
+    result, unique_id = dish_leaf_node.SetStandbyFPMode()
+
+    logger.debug("Command id: %s | Returned result: %s", unique_id, result)
+    assert result[0] == ResultCode.QUEUED
+
+    lrcr_event_id = dish_leaf_node.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandResult"],
+    )
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id[0], COMMAND_COMPLETED),
+        lookahead=2,
+    )
+
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_FP),
+        lookahead=5,
+    )
+
+    dish_master.SetDelayInfo(json.dumps({"Abort": 35}))
+    result, unique_id = dish_leaf_node.Abort()
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id[0], COMMAND_TIMEOUT),
+        lookahead=3,
+    )
+    dish_master.ResetDelayInfo()
+    dish_leaf_node.unsubscribe_event(lrcr_event_id)
+    dish_leaf_node.unsubscribe_event(dishmode_event_id)
+    tear_down(dish_leaf_node, dish_master, group_callback)
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_abort_exception(tango_context, group_callback):
+    abort_exception(
+        tango_context,
+        DISH_LEAF_NODE_DEVICE,
+        group_callback,
+    )
+
+
+def abort_exception(tango_context, dishln_name, group_callback):
+    dev_factory = DevFactory()
+    dish_leaf_node = dev_factory.get_device(dishln_name)
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    dish_master.SetDirectDishMode(DishMode.STANDBY_LP)
+    dishmode_event_id = dish_leaf_node.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["dishMode"],
+    )
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_LP),
+        lookahead=2,
+    )
+
+    result, unique_id = dish_leaf_node.SetStandbyFPMode()
+
+    logger.debug("Command id: %s | Returned result: %s", unique_id, result)
+    assert result[0] == ResultCode.QUEUED
+
+    lrcr_event_id = dish_leaf_node.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandResult"],
+    )
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id[0], COMMAND_COMPLETED),
+        lookahead=2,
+    )
+
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_FP),
+        lookahead=5,
+    )
+
+    ERROR_PROPAGATION_DEFECT = json.dumps(
+        {
+            "enabled": True,
+            "fault_type": FaultType.LONG_RUNNING_EXCEPTION,
+            "error_message": "Exception occured, command failed.",
+            "result": ResultCode.FAILED,
+        }
+    )
+
+    dish_master.SetDefective(ERROR_PROPAGATION_DEFECT)
+    result, unique_id = dish_leaf_node.Abort()
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id[0], COMMAND_FAILED),
+        lookahead=8,
+    )
+
+    RESET_DEFECT = json.dumps(
+        {
+            "enabled": False,
+            "fault_type": FaultType.FAILED_RESULT,
+            "error_message": "Default exception.",
+            "result": ResultCode.FAILED,
+        }
+    )
+    dish_master.SetDefective(RESET_DEFECT)
+    dish_leaf_node.unsubscribe_event(lrcr_event_id)
+    dish_leaf_node.unsubscribe_event(dishmode_event_id)
+    tear_down(dish_leaf_node, dish_master, group_callback)
