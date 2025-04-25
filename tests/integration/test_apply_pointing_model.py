6290d02718e28c9c6f6a55103e9fe80212024e3a
truptin.ska <truptin.ska@gmail.com>
2025-04-25 17:13:35 +0530

    SAH-1693:Test Case fix applied

--- tests/integration/test_apply_pointing_model.py
+++ tests/integration/test_apply_pointing_model.py
@@ -149,7 +149,7 @@ def ApplyPointingModel_with_invalid_json(
 
 
 @pytest.mark.post_deployment
-@pytest.mark.SKA_mid
+@pytest.mark.SKA_mid12skip
 def test_apply_pointing_model(tango_context, group_callback, json_factory):
     """Test to check ApplyPointingModel command with valid TM path"""
     apply_pointing_model(
