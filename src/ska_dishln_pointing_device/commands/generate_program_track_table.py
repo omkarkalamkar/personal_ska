50413751f0392e2532b37c4bdd2e330741f6b039
pranavmaturkar.ska <pranavmaturkar.ska@gmail.com>
2025-06-30 20:44:03 +0530

    HM-706:added fix for failed test case

--- src/ska_dishln_pointing_device/commands/generate_program_track_table.py
+++ src/ska_dishln_pointing_device/commands/generate_program_track_table.py
@@ -19,10 +19,6 @@ from ska_dishln_pointing_device.mapping_scan.point_mapping import (
 class GenerateProgramTrackTable:
     """Long-running version of GenerateProgramTrackTable."""
 
-    # def __init__(self, component_manager, logger: logging.Logger = None):
-    #     super().__init__(logger)
-    #     self.component_manager = component_manager
-
     def __init__(self, component_manager, logger: logging.Logger = None):
         self.logger = logger or logging.getLogger(__name__)
         self.component_manager = component_manager
@@ -35,56 +31,134 @@ class GenerateProgramTrackTable:
         This is a long running method for GenerateProgramTrackTable command,
         executes do hook, invokes command asynchronously.
         """
+        task_callback: Optional[Callable] = kwargs.get("task_callback")
 
-        task_callback: Optional[Callable] = kwargs.get("task_callback", None)
+        if task_callback:
+            task_callback(status=TaskStatus.IN_PROGRESS)
 
-        task_callback(status=TaskStatus.IN_PROGRESS)
-        return_code, message = self.do()
+        try:
+            return_code, message = self.do()
+        except Exception as ex:
+            self.logger.exception(
+                "Exception occurred in GenerateProgramTrackTable on %s: %s",
+                self.component_manager.dishln_pointing_device_name,
+                ex,
+            )
+            if task_callback:
+                task_callback(
+                    status=TaskStatus.COMPLETED,
+                    result=(ResultCode.FAILED, str(ex)),
+                    exception=ex,
+                )
+            return ResultCode.FAILED, str(ex)
+
         self.logger.debug(
-            "Updating Task status with Result: %s ,Message: %s",
-            ResultCode(return_code),
+            "Updating Task status with Result: %s , Message: %s",
+            return_code,
             message,
         )
-        if return_code == ResultCode.FAILED:
-            task_callback(
-                status=TaskStatus.COMPLETED,
-                result=(return_code, message),
-                exception=message,
-            )
-        else:
+
+        if task_callback:
             task_callback(
                 status=TaskStatus.COMPLETED,
                 result=(ResultCode.OK, message),
             )
 
+        return ResultCode.OK, message
+
     def do(self) -> Tuple[ResultCode, str]:
-        """Executes the command asynchronously."""
+        """
+        Executes the GenerateProgramTrackTable command logic.
+        """
+        self.logger.info(
+            "Executing GenerateProgramTrackTable command on %s",
+            self.component_manager.dishln_pointing_device_name,
+        )
 
-        try:
-            self.logger.info(
-                "Executing GenerateProgramTrackTable command on %s",
-                self.component_manager.dishln_pointing_device_name,
-            )
+        with self.component_manager.track_thread_lock:
+            self.component_manager.mapping_scan_event.clear()
 
-            with self.component_manager.track_thread_lock:
-                self.component_manager.mapping_scan_event.clear()
+        self.component_manager.current_mapping_scan_obj = FixedMappingScan(
+            pattern_name="fixed",
+            component_manager=self.component_manager,
+            logger=self.logger,
+        )
 
-            self.component_manager.current_mapping_scan_obj = FixedMappingScan(
-                pattern_name="fixed",
-                component_manager=self.component_manager,
-                logger=self.logger,
-            )
+        current_scan_obj = self.component_manager.current_mapping_scan_obj
+        current_scan_obj.set_target_and_start_process()
 
-            current_scan_obj = self.component_manager.current_mapping_scan_obj
-            current_scan_obj.set_target_and_start_process()
+        return ResultCode.OK, "Command Completed"
 
-        except Exception as exception:
-            self.logger.exception(
-                " Exception occurred "
-                + "in GenerateProgramTrackTable command on %s ,"
-                + "Exception: %s",
-                self.component_manager.dishln_pointing_device_name,
-                exception,
-            )
-            raise exception
-        return [ResultCode.OK], ["Command Completed"]
+
+# class GenerateProgramTrackTable:
+#     """Long-running version of GenerateProgramTrackTable."""
+
+#     # def __init__(self, component_manager, logger: logging.Logger = None):
+#     #     super().__init__(logger)
+#     #     self.component_manager = component_manager
+
+#     def __init__(self, component_manager, logger: logging.Logger = None):
+#         self.logger = logger or logging.getLogger(__name__)
+#         self.component_manager = component_manager
+
+#     def generate_program_track_table(
+#         self,
+#         **kwargs,
+#     ) -> Tuple[ResultCode, str]:
+#         """
+#         This is a long running method for GenerateProgramTrackTable command,
+#         executes do hook, invokes command asynchronously.
+#         """
+
+#         task_callback: Optional[Callable] = kwargs.get("task_callback", None)
+
+#         task_callback(status=TaskStatus.IN_PROGRESS)
+#         return_code, message = self.do()
+#         self.logger.debug(
+#             "Updating Task status with Result: %s ,Message: %s",
+#             ResultCode(return_code),
+#             message,
+#         )
+#         if return_code == ResultCode.FAILED:
+#             task_callback(
+#                 status=TaskStatus.COMPLETED,
+#                 result=(return_code, message),
+#                 exception=message,
+#             )
+#         else:
+#             task_callback(
+#                 status=TaskStatus.COMPLETED,
+#                 result=(ResultCode.OK, message),
+#             )
+
+#     def do(self) -> Tuple[ResultCode, str]:
+#         """Executes the command asynchronously."""
+
+#         try:
+#             self.logger.info(
+#                 "Executing GenerateProgramTrackTable command on %s",
+#                 self.component_manager.dishln_pointing_device_name,
+#             )
+
+#             with self.component_manager.track_thread_lock:
+#                 self.component_manager.mapping_scan_event.clear()
+
+#           self.component_manager.current_mapping_scan_obj = FixedMappingScan(
+#                 pattern_name="fixed",
+#                 component_manager=self.component_manager,
+#                 logger=self.logger,
+#             )
+
+#           current_scan_obj = self.component_manager.current_mapping_scan_obj
+#             current_scan_obj.set_target_and_start_process()
+
+#         except Exception as exception:
+#             self.logger.exception(
+#                 " Exception occurred "
+#                 + "in GenerateProgramTrackTable command on %s ,"
+#                 + "Exception: %s",
+#                 self.component_manager.dishln_pointing_device_name,
+#                 exception,
+#             )
+#             raise exception
+#         return [ResultCode.OK], ["Command Completed"]
