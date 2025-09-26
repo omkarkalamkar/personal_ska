c7d437fae16ec0a3c0ae8261690d8e7e3ee155e1
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-09-26 16:49:37 +0530

    HM-756: Reverting chnages

--- tests/unit/test_dishln_pointing_device_cm.py
+++ tests/unit/test_dishln_pointing_device_cm.py
@@ -267,7 +267,7 @@ def test_dish_pointing_schedular_length(cm_pointig_device, json_factory):
             logger=logger, component_manager=cm
         )
         generate_program_track_table.do()
-        while not cm.pointing_program_track_table and timeout < 8:
+        while not cm.pointing_program_track_table and timeout < 5:
             time.sleep(1)
             timeout += 1
         time.sleep(2)
