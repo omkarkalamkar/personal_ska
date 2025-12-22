9cd1b57a5be712d0c5804d1bd32ae46edb914ea7
manthan <manthan.ska@gmail.com>
2025-12-22 13:01:06 +0530

    SAH-1829: Revert change

--- tests/integration/test_dishln_abort_command.py
+++ tests/integration/test_dishln_abort_command.py
@@ -177,6 +177,8 @@ def abort_while_configuring(
         lookahead=5,
     )
 
+    assert dish_leaf_node.pointingState == PointingState.READY
+    
     result_config, unique_id_config = dish_leaf_node.Configure(
         configure_input_str
     )
@@ -184,11 +186,11 @@ def abort_while_configuring(
     logger.info(
         f"Command ID: {unique_id_config} Returned result: {result_config}"
     )
-    if dish_leaf_node.pointingState != PointingState.READY:
-        group_callback["pointingState"].assert_change_event(
-            (PointingState.READY),
-            lookahead=6,
-        )
+    # if dish_leaf_node.pointingState != PointingState.READY:
+    #     group_callback["pointingState"].assert_change_event(
+    #         (PointingState.READY),
+    #         lookahead=6,
+    #     )
 
     group_callback["dishMode"].assert_change_event(
         (DishMode.OPERATE),
