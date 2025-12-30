303392ee84a2c8ebe5ce9a77eef747582e0870b4
pranavmaturkar.ska <pranavmaturkar.ska@gmail.com>
2025-12-30 10:41:13 +0530

    HM-807:Fix for test

--- src/ska_tmc_dishleafnode/manager/health_rules.py
+++ src/ska_tmc_dishleafnode/manager/health_rules.py
@@ -8,10 +8,10 @@ from ska_control_model import HealthState
 HEALTH_RULES = {
     HealthState.OK: [
         Rule("kvalue_validation_result == 'OK'"),
+        Rule("$all([gpm != 'FAILED' for gpm in gpm_validation_result ])"),
     ],
     HealthState.FAILED: [
         Rule("kvalue_validation_result != 'OK'"),
-        Rule("$all([gpm != 'FAILED' for gpm in gpm_validation_result ])"),
     ],
     HealthState.DEGRADED: [
         Rule("$any([gpm == 'FAILED' for gpm in gpm_validation_result ])"),
