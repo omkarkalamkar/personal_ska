dbfc2d808c67f828dd3280a6cab272561e387909
ganesh.shinde <ganeshshinde.ska@gmail.com>
2026-01-08 15:46:32 +0530

    SAH-1842: minor fix to rules

--- src/ska_tmc_dishleafnode/manager/health_rules.py
+++ src/ska_tmc_dishleafnode/manager/health_rules.py
@@ -18,7 +18,7 @@ HEALTH_RULES = {
         # Case 1: Specific band configured and fully capable
         Rule(
             "k_value_validation_result.result_code == 'OK' and "
-            "$all(gpm != 'FAILED' for gpm in gpm_validation_result.result) "
+            "$all([gpm != 'FAILED' for gpm in gpm_validation_result.result]) "
             "and receiver_band not in {'NONE', 'UNKNOWN'} and "
             "band_capability_data.band_capabilities[receiver_band] "
             f"in {GOOD_CAPABILITY_STATES}"
@@ -26,20 +26,22 @@ HEALTH_RULES = {
         # Case 2: No band configured, all bands good
         Rule(
             "k_value_validation_result.result_code == 'OK' and "
-            "$all(gpm != 'FAILED' for gpm in gpm_validation_result.result) "
+            "$all([gpm != 'FAILED' for gpm in gpm_validation_result.result]) "
             "and receiver_band in {'NONE', 'UNKNOWN'} and "
-            "$all(state in "
+            "$all([state in "
             f"{GOOD_CAPABILITY_STATES} for state in "
-            "band_capability_data.band_capability_values)"
+            "band_capability_data.band_capability_values])"
         ),
     ],
     HealthState.DEGRADED: [
         # Any GPM failed
-        Rule("$any(gpm == 'FAILED' for gpm in gpm_validation_result.result)"),
+        Rule(
+            "$any([gpm == 'FAILED' for gpm in gpm_validation_result.result])"
+        ),
         # Configured band not fully capable (KValue and GPM are OK)
         Rule(
             "k_value_validation_result.result_code == 'OK' and "
-            "$all(gpm != 'FAILED' for gpm in gpm_validation_result.result) "
+            "$all([gpm != 'FAILED' for gpm in gpm_validation_result.result]) "
             "and receiver_band not in {'NONE', 'UNKNOWN'} and "
             "band_capability_data.band_capabilities[receiver_band] "
             f"not in {GOOD_CAPABILITY_STATES}"
@@ -47,14 +49,14 @@ HEALTH_RULES = {
         # No band configured, but some bands are bad (partial capability)
         Rule(
             "k_value_validation_result.result_code == 'OK' and "
-            "$all(gpm != 'FAILED' for gpm in gpm_validation_result.result) "
+            "$all([gpm != 'FAILED' for gpm in gpm_validation_result.result]) "
             "and receiver_band in {'NONE', 'UNKNOWN'} and "
-            "$any(state in "
+            "$any([state in "
             f"{GOOD_CAPABILITY_STATES} for state in "
-            "band_capability_data.band_capability_values) and "
-            "$any(state not in "
+            "band_capability_data.band_capability_values]) and "
+            "$any([state not in "
             f"{GOOD_CAPABILITY_STATES} for state in "
-            "band_capability_data.band_capability_values)"
+            "band_capability_data.band_capability_values])"
         ),
     ],
     HealthState.FAILED: [
@@ -63,9 +65,9 @@ HEALTH_RULES = {
         # No band configured and ALL bands unavailable
         Rule(
             "receiver_band in {'NONE', 'UNKNOWN'} and "
-            "$all(state not in "
+            "$all([state not in "
             f"{GOOD_CAPABILITY_STATES} for state in "
-            "band_capability_data.band_capability_values)"
+            "band_capability_data.band_capability_values])"
         ),
     ],
     HealthState.UNKNOWN: [
