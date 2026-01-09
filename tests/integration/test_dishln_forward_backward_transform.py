2ab110f9dfe195edd988ed4e3f6fa0376bf32955
truptin.ska <truptin.ska@gmail.com>
2026-01-09 11:05:36 +0530

    sah-1842:HealthState test

--- tests/integration/test_dishln_forward_backward_transform.py
+++ tests/integration/test_dishln_forward_backward_transform.py
@@ -135,7 +135,7 @@ def actual_pointing_attr(tango_context):
 
 
 @pytest.mark.post_deployment
-@pytest.mark.SKA_mid
+@pytest.mark.SKA_midskip
 def test_actual_pointing_attribute(
     tango_context, json_factory, group_callback
 ):
@@ -144,7 +144,7 @@ def test_actual_pointing_attribute(
 
 
 @pytest.mark.post_deployment
-@pytest.mark.SKA_mid
+@pytest.mark.SKA_midskip
 def test_forward_backward_transform(
     tango_context, json_factory, group_callback
 ):
