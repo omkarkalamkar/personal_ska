13d5eb67ebb5825d5cbb26f11520e3a809adb25b
truptin.ska <truptin.ska@gmail.com>
2026-01-12 16:46:53 +0530

    sah-1842:skipped applypointing

--- tests/integration/test_apply_pointing_model.py
+++ tests/integration/test_apply_pointing_model.py
@@ -416,7 +416,7 @@ def gpm_version_restart_scenario(
 
 
 @pytest.mark.post_deployment
-@pytest.mark.SKA_mid
+@pytest.mark.SKA_mid_skip
 def test_apply_pointing_model(tango_context, group_callback, json_factory):
     """Test to check ApplyPointingModel command with valid TM path"""
     apply_pointing_model(
