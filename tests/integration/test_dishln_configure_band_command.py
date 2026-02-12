6abc07735a0a0f9dcad3feb476193255fc713aac
pranavmaturkar.ska <pranavmaturkar.ska@gmail.com>
2026-02-13 00:14:41 +0530

    HM-840:fix test increase lookahead

--- tests/integration/test_dishln_configure_band_command.py
+++ tests/integration/test_dishln_configure_band_command.py
@@ -70,7 +70,7 @@ def configureband_command(tango_context, dishln_name, group_callback, argin):
     )
     group_callback["longRunningCommandResult"].assert_change_event(
         (unique_id_op[0], COMMAND_COMPLETED),
-        lookahead=7,
+        lookahead=15,
     )
     argin_reciever_band = (
         json.loads(argin).get("dish", {}).get("receiver_band", "")
