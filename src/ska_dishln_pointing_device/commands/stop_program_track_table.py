6a7f0f65bd6f49c413de7b75aead191225792980
Shraddha Bhat <shraddhabhat.ska@gmail.com>
2026-03-24 15:50:13 +0530

    HM-849: Fix test coverage

--- src/ska_dishln_pointing_device/commands/stop_program_track_table.py
+++ src/ska_dishln_pointing_device/commands/stop_program_track_table.py
@@ -38,6 +38,8 @@ class StopProgramTrackTable(FastCommand):
                 self.component_manager.track_table_thread
                 and self.component_manager.track_table_thread.is_alive()
             ):
+                # The track_table_thread is joined with a short timeout to
+                # prevent the Stop command from triggering a Tango timeout
                 self.component_manager.track_table_thread.join(timeout=2.8)
             self.component_manager.update_pointing_program_track_table([])
             self.component_manager.current_mapping_scan_obj = None
