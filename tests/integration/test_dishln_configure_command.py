bd62cfed7541727e36d27f968780a25e701f6696
yogeshk.ska <yogeshk.ska@gmail.com>
2026-03-31 12:30:08 +0530

    HM-866: test case added

--- tests/integration/test_dishln_configure_command.py
+++ tests/integration/test_dishln_configure_command.py
@@ -1031,3 +1031,130 @@ def test_partial_configure_with_ie_wo_ie(
             main_configure,
             partial_configure,
         )
+
+
+def validate_mattieu_pattern_configure(
+    tango_context,
+    dishln_name,
+    group_callback,
+    configure_input_str,
+):
+    logger.info(f"{tango_context}")
+    dev_factory = DevFactory()
+    dish_leaf_node = dev_factory.get_device(dishln_name)
+    dish_master = dev_factory.get_device(DISH_MASTER_DEVICE)
+    dishln_pointing_device = dev_factory.get_device(DISHLN_POINTING_DEVICE)
+    dish_master.SetDirectDishMode(DishMode.STANDBY_LP)
+    dishmode_event_id = dish_leaf_node.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["dishMode"],
+    )
+    pointingstate_event_id = dish_leaf_node.subscribe_event(
+        "pointingState",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["pointingState"],
+    )
+    dishpd_event_id = dishln_pointing_device.subscribe_event(
+        "pointingProgramTrackTable",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["pointingProgramTrackTable"],
+    )
+
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_LP),
+        lookahead=2,
+    )
+
+    result_fp, unique_id_fp = dish_leaf_node.SetStandbyFPMode()
+    time.sleep(1)
+    assert result_fp[0] == ResultCode.QUEUED
+
+    lrcr_event_id = dish_leaf_node.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        group_callback["longRunningCommandResult"],
+    )
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_fp[0], COMMAND_COMPLETED),
+        lookahead=6,
+    )
+
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.STANDBY_FP),
+        lookahead=6,
+    )
+
+    result_config, unique_id_config = dish_leaf_node.Configure(
+        configure_input_str
+    )
+
+    assert result_config[0] == ResultCode.QUEUED
+    logger.info(
+        f"Command ID: {unique_id_config} Returned result: {result_config}"
+    )
+
+    group_callback["longRunningCommandResult"].assert_change_event(
+        (unique_id_config[0], COMMAND_COMPLETED),
+        lookahead=6,
+    )
+    group_callback["pointingState"].assert_change_event(
+        (PointingState.SLEW),
+        lookahead=6,
+    )
+    group_callback["dishMode"].assert_change_event(
+        (DishMode.OPERATE),
+        lookahead=6,
+    )
+    wait_and_validate_attribute_value_available(
+        dish_leaf_node, "pointingState", PointingState.TRACK, timeout=30
+    )
+
+    # Validate number of program track table entries is 150
+    assert (
+        len(json.loads(dishln_pointing_device.pointingProgramTrackTable))
+        == NUMBER_OF_PROGRAM_TRACK_TABLE_ENTRIES
+    )
+    configure_str = json.loads(configure_input_str)
+    if configure_str.get("pointing", {}).get("field", ""):
+        # Allow some time for 5 PTT to get generated for TLE tracking
+        # For 50 enrtries of one track table, its taking around
+        # 2+ seconds
+        dish_leaf_node.TrackStop()
+        wait_and_validate_attribute_value_available(
+            dishln_pointing_device,
+            "pointingProgramTrackTable",
+            "[]",
+            timeout=60,
+        )
+    else:
+        result_config, unique_id_config = dish_leaf_node.TrackStop()
+        group_callback["longRunningCommandResult"].assert_change_event(
+            (unique_id_config[0], COMMAND_COMPLETED),
+            lookahead=6,
+        )
+        group_callback["pointingState"].assert_change_event(
+            (PointingState.READY),
+            lookahead=6,
+        )
+        group_callback["pointingProgramTrackTable"].assert_change_event(
+            ("[]"),
+            lookahead=8,
+        )
+
+    dish_leaf_node.unsubscribe_event(dishmode_event_id)
+    dish_leaf_node.unsubscribe_event(pointingstate_event_id)
+    dish_leaf_node.unsubscribe_event(lrcr_event_id)
+    dishln_pointing_device.unsubscribe_event(dishpd_event_id)
+    tear_down(dish_leaf_node, dish_master, group_callback)
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_mattieu_pattern(tango_context, group_callback, json_factory):
+    """ """
+    main_configure = json_factory("dln_configure_pvt")
+    validate_mattieu_pattern_configure(
+        tango_context, DISH_LEAF_NODE_DEVICE, group_callback, main_configure
+    )
